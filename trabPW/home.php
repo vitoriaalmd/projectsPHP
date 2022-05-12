a8af50f8707057e60996b559a525aae034db45da
vitoriaalmd <vitoriaalmeid54@gmail.com>
2022-05-12 14:53:08 -0300

    alteração 12.05.22

--- trabPW/home.php
+++ trabPW/home.php
@@ -12,17 +12,17 @@
     <title>Document</title>
 </head>
 <body>
-<div class="container col-md-6 offset-md-3">
-		<h1>Formulário de Matrícula</h1>   
+<div class="container">
+		<center> <h1>Formulário de Matrícula</h1></center>
         <form method="post" action="salvar.php">
-        <h2>Dados do(a) estudante</h2> 
-        <div class="row g-3">
+        <center><h2>Dados do(a) estudante</h2> </center>
+        <div class="row mb-3">
             <div class="col-sm-12">
                 Nome Completo: <input type="text" placeholder="Nome" class="form-control form-control-sm" required>
             </div>
         </div>
-        <div class row mb-3>
-            <div class="col-sm-5">
+        <div class ="row mb-3">
+            <div class="col-sm-3">
                 <label>Sexo:</label> <br>
                 <div class="form-check form-check-inline" required>
                     <input class="form-check-input" type="radio" id="inlineRadio1" value="option1">
@@ -32,6 +32,21 @@
                 <input class="form-check-input" type="radio" id="inlineRadio2" value="option2">
                   Masculino
                 </div>
+            </div>
+                <div class="col-sm-2">
+                    <label>Cartão de Vacinação:</label> <br>
+                    <div class="form-check form-check-inline" required>
+                    <input class="form-check-input" type="radio" id="inlineRadio12" value="option3">
+                    Sim
+                </div>
+                <div class="form-check form-check-inline" > 
+                <input class="form-check-input" type="radio" id="inlineRadio21" value="option4">
+                  Não
+                </div>
+                </div>
+                <div class="col-sm-4">
+                Nº do Cartão SUS: <input type="number" placeholder="" class="form-control form-control-sm" required>  
+                </div>
             <div class="col-sm-3">
                  <label>Data de Nascimento:</label>
                  <div class="input-group"  required> 
@@ -39,6 +54,7 @@
                  </div>
                  </div>
             
+        </div>
         </div>
 </body>
 </html>
