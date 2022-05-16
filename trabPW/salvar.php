ffd8777bafc2c67f389cc89dd1b763193540089f
vitoriaalmd <vitoriaalmeid54@gmail.com>
2022-05-15 22:01:45 -0300

    .

--- trabPW/salvar.php
+++ trabPW/salvar.php
@@ -1,7 +1,7 @@
 <?php 
-include 'conexaoBD.php';
-$nome = $_POST['nome'];
-$nome2 = $_POST['nome2'];
+//include 'conexaoBD.php';
+//$nome = $_POST['nome'];
+//$nome2 = $_POST['nome2'];
 //$salvar = mysql_query($conexaoBD, "INSERT INTO usuario(nome,nome2)) VALUES('$nome', '$nome2')");
 //if ($salvar){
     header ('location: index.php')
